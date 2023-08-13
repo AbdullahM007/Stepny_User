f7a6f86000db891f25e822135b4b439aedca4a24
JAMI-IT <103955140+JAMI-IT@users.noreply.github.com>
2023-08-13 22:00:13 +0500

    change teh styling

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -7,13 +7,13 @@ import HomeSearch from '../../components/HomeSearch';
 const HomeScreen = props => {
   return (
     <View>
-      <View style={{height: Dimensions.get('window').height - 330}}>
+      <View style={{height: Dimensions.get('window').height}}>
         <HomeMap />
       </View>
 
       {/* <InfoMessage/> */}
       {/* <Text>index</Text> */}
-      <HomeSearch />
+      {/* <HomeSearch /> */}
     </View>
   );
 };
