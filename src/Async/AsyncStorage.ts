34677e1e9ec342f0c11edbf51f9827db80b957d0
JAMI-IT <103955140+JAMI-IT@users.noreply.github.com>
2023-08-14 21:07:17 +0500

    adding async storage

---
+++ src/Async/AsyncStorage.ts
@@ -0,0 +1,41 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const getStorageData = async (key: string) => {
+  try {
+    let data = await AsyncStorage.getItem(key);
+    if (data) return JSON.parse(data)
+    else return null
+  } catch (error) {
+    return null;
+  }
+};
+
+export const storeData = async (key: string, value: any) => {
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error(error)
+  }
+};
+
+// export const getAppData = async () => {
+//   try {
+//     const value = await AsyncStorage.getItem(storageKey);
+//     if (value !== null) {
+//       // value previously stored
+//     }
+//   } catch (e) {
+//     // error reading value
+//   }
+// };
+
+export const removeData = async () => {
+  try {
+    await AsyncStorage.removeItem('@key'); // Replace '@key' with the key of the data to be removed
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const removeStorageData = async (key: string) => {
+  return await AsyncStorage.removeItem(key);
+};
